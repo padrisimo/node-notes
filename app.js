7322c7eac6bb697bd569e8b981d04f77f38cd291
padrisimo <nopuedoparardemolar@outlook.com>
2017-12-29 22:01:21 +0100

    refactoring for reusability

--- app.js
+++ app.js
@@ -13,7 +13,15 @@ console.log('Command: ', command);
 console.log('Yargs : ', argv);
 
 if(command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    if (note){
+        console.log('Note created');
+        console.log('--');
+        console.log(`Title: ${note.title}`);
+        console.log(`Body: ${note.body}`);
+    } else {
+        console.log('note title taken');
+    }
 } else if (command === 'list'){
     notes.getAll();
 } else if( command === 'read'){
