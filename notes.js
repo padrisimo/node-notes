e22a881b8db8eaaef0a6ad33f6aa70e139fa75e0
padrisimo <chamizho@gmail.com>
2018-03-24 15:16:20 +0100

    reading notes and reusability

--- notes.js
+++ notes.js
@@ -2,7 +2,7 @@ console.log('Starting notes ');
 
 const fs = require('fs');
 
-fecthNotes = () => {
+fetchNotes = () => {
     try {
         var notesString = fs.readFileSync('notes-data.json');
         return JSON.parse(notesString);
@@ -17,7 +17,7 @@ var saveNotes = notes => {
 };
 
 var addNote = (title, body) => {
-    var notes = fecthNotes();
+    var notes = fetchNotes();
     var note = {
         title,
         body
@@ -37,20 +37,29 @@ var getAll = () => {
 }
 
 var getNote = (title) => {
-    console.log('Getting note', title);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title );
+    return filteredNotes[0];
 }
 
 var removeNote = (title) => {
-    var notes = fecthNotes();
+    var notes = fetchNotes();
     var filteredNotes = notes.filter((note) => note.title !== title);
     saveNotes(filteredNotes);
 
     return notes.length !== filteredNotes.length;
 }
 
+var logNote  = (note) => {
+    console.log('--');
+    console.log(`Title: ${note.title}`);
+    console.log(`Body: ${note.body}`);
+} 
+
 module.exports = {
     addNote,
     getAll,
     getNote,
+    logNote,
     removeNote
 };
